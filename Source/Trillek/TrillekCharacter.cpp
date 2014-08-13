356bba065ac4cc99f72801858ee42eaece8314c1
ShaneDalton <leroy.dalton1@gmail.com>
2014-08-13 18:45:38 +1000

    Added Character Movement

--- Source/Trillek/TrillekCharacter.cpp
+++ Source/Trillek/TrillekCharacter.cpp
@@ -10,4 +10,65 @@ ATrillekCharacter::ATrillekCharacter(const class FPostConstructInitializePropert
 
 }
 
+void ATrillekCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void ATrillekCharacter::SetupPlayerInputComponent(UInputComponent* InputComponent)
+{
+	// set up gameplay key bindings
+	InputComponent->BindAxis("MoveForward", this, &ATrillekCharacter::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &ATrillekCharacter::MoveRight);
+	InputComponent->BindAxis("Turn", this, &ATrillekCharacter::AddControllerYawInput);
+	InputComponent->BindAxis("LookUp", this, &ATrillekCharacter::AddControllerPitchInput);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ATrillekCharacter::OnStartJump);
+	InputComponent->BindAction("Jump", IE_Released, this, &ATrillekCharacter::OnStartJump);
+
+}
+
+void ATrillekCharacter::MoveForward(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		// find out which way is forward
+		FRotator Rotation = Controller->GetControlRotation();
+		// limit pitch when walking or falling 
+		if (CharacterMovement->IsMovingOnGround() || CharacterMovement->IsFalling())
+		{
+			Rotation.Pitch = 0.0f;
+		}
+		// add movement in that direction
+		const FVector Direction = FRotationMatrix(Rotation).GetScaledAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+	void ATrillekCharacter::MoveRight(float Value)
+	{
+		if ((Controller != NULL) && (Value != 0.0f))
+		{
+			// find out which way is right
+			const FRotator Rotation = Controller->GetControlRotation();
+			const FVector Direction = FRotationMatrix(Rotation).GetScaledAxis(EAxis::Y);
+			// add movement in that direction
+			AddMovementInput(Direction, Value);
+		}
+	}
+
+	void ATrillekCharacter::OnStartJump()
+	{
+		bPressedJump = true;
+	}
+
+	void ATrillekCharacter::OnStopJump()
+	{
+		bPressedJump = false;
+	}
+
+
+
+
+
 
--- Source/Trillek/TrillekCharacter.h
+++ Source/Trillek/TrillekCharacter.h
@@ -13,6 +13,21 @@ class TRILLEK_API ATrillekCharacter : public ACharacter
 {
 	GENERATED_UCLASS_BODY()
 
-	
+	virtual void BeginPlay() OVERRIDE;
+
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) OVERRIDE;
+	/** handles moving forward/backward */
+	UFUNCTION()
+		void MoveForward(float Val);
+	/** handles strafing */
+	UFUNCTION()
+		void MoveRight(float Val);
+	/** sets jump flag when key is pressed */
+	UFUNCTION()
+		void OnStartJump();
+	/** clears jump flag when key is released */
+	UFUNCTION()
+		void OnStopJump();
 	
 };
