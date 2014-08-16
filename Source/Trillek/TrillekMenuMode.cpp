c882fe7e9813a2a280c69a9afc247543f16d92b3
ShaneDalton <leroy.dalton1@gmail.com>
2014-08-16 22:16:50 +1000

    Created TrillekMainMode class

---
+++ Source/Trillek/TrillekMenuMode.cpp
@@ -0,0 +1,13 @@
+
+
+#include "Trillek.h"
+#include "TrillekMenuMode.h"
+
+
+ATrillekMenuMode::ATrillekMenuMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/Trillek/TrillekMenuMode.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TrillekMenuMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRILLEK_API ATrillekMenuMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
